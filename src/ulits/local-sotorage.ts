eb0bf1fd6072104f3bec3af92f4608d065f715f1
rahulhasan31 <rahulhasan666@gmail.com>
2024-02-17 23:40:29 +0600

    task

--- src/ulits/local-sotorage.ts
+++ src/ulits/local-sotorage.ts
@@ -2,29 +2,28 @@ import Cookies from 'js-cookie';
 
 export const setToLocalStored=(key:string, token:string)=>{
        if(!key||typeof window===undefined){
-        return ""
+        return null
        }
        return localStorage.setItem(key, token as string)
 }
-export const getLocalStored=(key:string): string | null=>{
-       if(!key||typeof window===undefined){
-        return ""
-       }
-     return localStorage.getItem(key)
-}
-
+export const getLocalStored = (key: string): string | null => {
+  if (!key || typeof window === 'undefined') {
+    return null; // or handle this case appropriately
+  }
+  return localStorage.getItem(key);
+};
 
 
 
 export const setUserStore=(key:string, user:any)=>{
   if(!key||typeof window===undefined){
-    return ""
+    return null
    }
  return localStorage.setItem(key, user as any)
 }
 export const setCookiesToken=(key:string, token:string)=>{
   if(!key||typeof window===undefined){
-    return ""
+    return null
    }
  return Cookies.set(key, token as string)
 }
